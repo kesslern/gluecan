548c530c56d0e7551455db7e3d016f799f5aca67
Nathan Kessler <nathant93@gmail.com>
2019-06-28 16:12:25 -0400

    Fetch pastes with default password

--- gluecan-frontend/src/App.js
+++ gluecan-frontend/src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Typography from '@material-ui/core/Typography'
 import './App.css';
 
 function App() {
+  const [pastes, setPastes] = useState(null)
+
+  const loadPastes = useCallback(() => {
+    fetch('/api/pastes', { headers: {'X-Auth': 'change_me'} })
+      .then(it => it.json())
+      .then(it => {
+        console.log(it)
+        setPastes(it)
+      })
+      .catch(() => console.log("error"))
+  }, [setPastes])
+
   return (
     <div className="App">
       <CssBaseline/>
-      <Button variant="contained" color="primary">
-        Hello World
+      <Button variant="contained" color="primary" onClick={loadPastes}>
+        Load Pastes
       </Button>
+      {pastes && pastes.map(paste =>
+        <div key={paste.id}>
+          <div>{paste.id}</div>
+          <div>{paste.text}</div>
+        </div>
+      )}
       <Typography>Hello, world!</Typography>
     </div>
   );
