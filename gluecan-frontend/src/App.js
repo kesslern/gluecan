2d90479fa3b7c604af9d7200259afc9bb3158f5c
Nathan Kessler <nathant93@gmail.com>
2019-06-28 16:56:46 -0400

    Add login form

--- gluecan-frontend/src/App.js
+++ gluecan-frontend/src/App.js
@@ -1,26 +1,46 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react'
 import Button from '@material-ui/core/Button'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import Typography from '@material-ui/core/Typography'
-import './App.css';
+import './App.css'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   const [pastes, setPastes] = useState(null)
+  const [password, setPassword] = useState('')
+
+  const formSubmit = useCallback((event) => {
+    event.preventDefault()
+    console.log("Submit " + password)
+  }, [password])
+
+  const updatePassword = useCallback(event => {
+    setPassword(event.target.value)
+  }, [setPassword])
 
   return (
     <div className="App">
       <CssBaseline/>
-      <PasteLoader setPastes={setPastes}/>
-      <Button variant="contained" color="primary">
-        Hi
-      </Button>
-      {pastes && pastes.map(paste =>
+      <Typography variant="h5" color="textSecondary">GlueCan Administration</Typography>
+      <Paper component={"form"} onSubmit={formSubmit}>
+        <TextField
+          id="password"
+          label="Password"
+          margin="normal"
+          variant="outlined"
+          type="password"
+          value={password}
+          onChange={updatePassword}
+        />
+        <Button variant="contained" color="primary">Go</Button>
+      </Paper>
+      {pastes ? pastes.map(paste =>
         <div key={paste.id}>
           <div>{paste.id}</div>
           <div>{paste.text}</div>
         </div>
-      )}
-      <Typography>Hello, world!</Typography>
+      ) : <PasteLoader setPastes={setPastes}/> }
     </div>
   );
 }
@@ -28,7 +48,7 @@ function App() {
 function PasteLoader({ setPastes }) {
 
   useEffect(() => {
-    fetch('/api/pastes', { headers: {'X-Auth': 'change_me'} })
+    fetch('/api/pastes', { headers: { 'X-Auth': 'change_me' } })
       .then(it => it.json())
       .then(it => {
         console.log(it)
