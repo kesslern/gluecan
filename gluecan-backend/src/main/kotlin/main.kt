261eb19d7c768fd6358f32b3977b716a4651ee05
Nathan Kessler <nathant93@gmail.com>
2019-06-28 09:32:57 -0400

    Get pastes by ID

--- gluecan-backend/src/main/kotlin/main.kt
+++ gluecan-backend/src/main/kotlin/main.kt
@@ -1,15 +1,11 @@
 package us.kesslern
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import io.javalin.Javalin
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.dao.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
-import us.kesslern.Pastes.language
-import us.kesslern.Pastes.text
-import us.kesslern.Pastes.views
 import java.sql.Connection
 
 fun main() {
@@ -28,14 +24,26 @@ fun main() {
 
     app.get("/api/pastes") {
         transaction {
-            // it.json(Pastes.selectAll().map(ResultRow::toPaste))
-            it.json(Pastes.select {  Pastes.id eq 2 }.first().toPaste())
+            it.json(PasteDBO.all().map(PasteDBO::toPaste))
+        }
+    }
+
+    app.get("/api/pastes/:id") { ctx ->
+        transaction {
+            val paste = PasteDBO.findById(ctx.pathParam(":id").toInt())?.toPaste()
+
+            if (paste != null) {
+                ctx.json(paste)
+            } else {
+                ctx.status(410)
+            }
+
         }
     }
 
     app.post("/api/pastes") {
         transaction {
-            Pastes.insert {
+            PastesTable.insert {
                 it[text] = "foobar"
             }
         }
@@ -44,17 +52,26 @@ fun main() {
     app.start(8080)
 }
 
-data class RawPaste(
+data class Paste(
     val id: Int,
     val views: Int?,
     val language: String?,
     val text: String
 )
 
-object Pastes : IntIdTable() {
+object PastesTable : IntIdTable() {
     val views = integer("views")
-    val language = text("language")
+    val language = text("language").nullable()
     val text = text("text")
 }
 
-fun ResultRow.toPaste() = RawPaste(this[views], this[language], this[text])
+class PasteDBO(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<PasteDBO>(PastesTable)
+
+    var views by PastesTable.views
+    var language by PastesTable.language
+    var text by PastesTable.text
+}
+
+fun PasteDBO.toPaste() = Paste(this.id.value, this.views, this.language, this.text)
+
