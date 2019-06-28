fe79e43a7c233153cbab4b913aa7b82314f9dda1
Nathan Kessler <nathant93@gmail.com>
2019-06-28 15:56:53 -0400

    Add material-ui to admin frontend

--- gluecan-frontend/src/App.js
+++ gluecan-frontend/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline/>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      <Typography>Hello, world!</Typography>
     </div>
   );
 }
