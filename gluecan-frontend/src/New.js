1d9c412836b63748fde33e2feaf66a97f76485fe
Nathan Kessler <nathant93@gmail.com>
2019-07-09 23:35:40 -0400

    Style textarea and submit button for new route

--- gluecan-frontend/src/New.js
+++ gluecan-frontend/src/New.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import makeStyles from '@material-ui/styles/makeStyles'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    width: '100%',
+    height: '100%',
+    padding: theme.spacing(2),
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  textarea: {
+    resize: 'none',
+    width: '100%',
+    flexGrow: 1,
+  },
+  buttonContainer: {
+    marginTop: theme.spacing(2),
+    display: 'flex',
+    flexDirection: 'row-reverse',
+  },
+}))
 
 function New() {
-  return <div>new paste</div>
+  const classes = useStyles()
+
+  return (
+    <div className={classes.container}>
+      <Paper
+        component="textarea"
+        className={classes.textarea}
+        placeholder="Your text here..."
+      ></Paper>
+      <div className={classes.buttonContainer}>
+        <Button color="primary" variant="contained">
+          Submit
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default New
