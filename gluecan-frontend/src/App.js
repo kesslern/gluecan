9c02561a1ddbb5de0526c1e3b44381f98cf564c4
Nathan Kessler <nathant93@gmail.com>
2019-07-09 23:20:25 -0400

    Add new paste button & component

--- gluecan-frontend/src/App.js
+++ gluecan-frontend/src/App.js
@@ -4,6 +4,8 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import LoginForm from './LoginForm'
 import Pastes from './Pastes'
 import { Route, Redirect, Switch } from 'react-router-dom'
+import { Link as RouterLink } from 'react-router-dom'
+import Link from '@material-ui/core/Link'
 import { useSelector } from 'react-redux'
 import history from './state/history'
 import { ConnectedRouter } from 'connected-react-router'
@@ -12,6 +14,8 @@ import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import AppBar from '@material-ui/core/AppBar'
 import Typography from '@material-ui/core/Typography'
 import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import New from './New'
 
 const useStyles = makeStyles(theme => ({
   contentBox: {
@@ -30,7 +34,14 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     minHeight: '100vh',
   },
+  newButton: {
+    marginLeft: 'auto',
+  },
 }))
+
+const LinkToNew = React.forwardRef((props, ref) => (
+  <RouterLink innerRef={ref} to={'/new'} {...props} />
+))
 
 function App() {
   const classes = useStyles()
@@ -43,6 +54,13 @@ function App() {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6">GlueCan</Typography>
+            <Button
+              component={LinkToNew}
+              className={classes.newButton}
+              color="inherit"
+            >
+              New
+            </Button>
           </Toolbar>
         </AppBar>
         <TransitionGroup className={classes.contentBox}>
@@ -51,6 +69,7 @@ function App() {
               <Switch location={location}>
                 <Route path="/pastes/:id" component={Pastes} />
                 <Route path="/pastes" component={Pastes} />
+                <Route path="/new" component={New} />
                 <Route exact path="/login" component={LoginForm} />
                 <Redirect to="/pastes" />
               </Switch>
