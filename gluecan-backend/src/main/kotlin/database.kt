297b5c69b3619bfa705fd2fd48f18c7966c86ff0
Nathan Kessler <nathant93@gmail.com>
2019-08-14 22:10:31 -0400

    Move database access to own methods

--- gluecan-backend/src/main/kotlin/database.kt
+++ gluecan-backend/src/main/kotlin/database.kt
@@ -13,8 +13,6 @@ fun initDatabase() {
     flyway.migrate()
 }
 
-fun Paste.toHtml(): String = StringEscapeUtils.escapeHtml(this.text)
-
 fun uniqueId(): Int {
     var id = randomId()
     val stmt = connection.prepareStatement("select id from Pastes where id = ?")
@@ -30,6 +28,81 @@ fun uniqueId(): Int {
 }
 
 fun randomId(): Int = Random.nextInt(0, 1000)
+
+fun getPasteText(id: Int): String? {
+    val stmt =
+        connection.prepareStatement("select PasteContent.text from Pastes inner join PasteContent on Pastes.text_id = PasteContent.rowid where Pastes.id = ?")
+    stmt.setInt(1, id)
+    val result = stmt.executeQuery()
+
+    return if (result.next()) {
+        result.getString("text")
+    } else null
+}
+
+fun getAllPastes(): List<Paste> {
+    val result = connection.createStatement().executeQuery("select Pastes.date, Pastes.views, Pastes.language, Pastes.id from Pastes")
+
+    val pastes = mutableListOf<Paste>()
+    while (result.next()) {
+        val id = result.getInt("id")
+        val date = result.getString("date")
+        val views = result.getInt("views")
+        val language = result.getString("language")
+        pastes += Paste(id, views, language, null, date)
+    }
+    return pastes
+}
+
+fun getPaste(id: Int): Paste? {
+    val retrieveStatement = connection.prepareStatement("select PasteContent.text, Pastes.date, Pastes.views, Pastes.language from Pastes inner join PasteContent on Pastes.text_id = PasteContent.rowid where Pastes.id = ?")
+    retrieveStatement.setInt(1, id)
+    val result = retrieveStatement.executeQuery()
+
+    return if (result.next()) {
+        val text = result.getString("text")
+        val date = result.getString("date")
+        val views = result.getInt("views")
+        val language = result.getString("language")
+        Paste(id, views, language, text, date)
+    } else {
+        null
+    }
+}
+
+fun deletePaste(id: Int): Boolean {
+    val stmt = connection.prepareStatement("delete from PasteContent where rowid=(select text_id from Pastes where id=?)")
+    stmt.setInt(1, id)
+    val result = stmt.executeUpdate()
+
+    return if (result > 0) {
+        val stmt2 = connection.prepareStatement("delete from Pastes where id=?")
+        stmt2.setInt(1, id)
+        stmt2.executeUpdate()
+        true
+    } else {
+        false
+    }
+}
+
+fun createPaste(text: String, language: String?): Int {
+    val id =  uniqueId()
+    var stmt = connection.prepareStatement("insert into PasteContent(text) values (?)")
+    stmt.setString(1, text)
+    stmt.executeUpdate()
+
+    stmt = connection.prepareStatement("insert into Pastes(id, text_id, language) values(?, last_insert_rowid(), ?)")
+    stmt.setInt(1, id)
+    stmt.setString(2, language)
+    stmt.executeUpdate()
+    return id
+}
+
+fun updateViewCount(id: Int) {
+    val updateStatement = connection.prepareStatement("update Pastes set views = views + 1 where id = ?")
+    updateStatement.setInt(1, id)
+    updateStatement.execute()
+}
 
 data class Paste(
     val id: Int,
--- gluecan-backend/src/main/kotlin/webapp.kt
+++ gluecan-backend/src/main/kotlin/webapp.kt
@@ -3,6 +3,9 @@ package us.kesslern
 import io.javalin.Javalin
 import io.javalin.core.security.SecurityUtil.roles
 import io.javalin.http.Context
+import org.apache.commons.lang.StringEscapeUtils
+
+fun Paste.toHtml(): String = StringEscapeUtils.escapeHtml(this.text)
 
 object Templater {
     fun template(paste: Paste): String {
@@ -13,20 +16,16 @@ object Templater {
             .replace("{{class}}", paste.language ?: "")
             .replace("{{date}}", paste.date)
             .replace("{{pasteHtml}}", paste.toHtml())
-
     }
 }
 
 fun viewRaw(ctx: Context) {
     val id = ctx.pathParam(":id").toInt()
-    val stmt = connection.prepareStatement("select PasteContent.text from Pastes inner join PasteContent on Pastes.text_id = PasteContent.rowid where Pastes.id = ?")
-    stmt.setInt(1, id)
+    val text = getPasteText(id)
 
-    val result = stmt.executeQuery()
-
-    if (result.next()) {
+    if (text !== null) {
         ctx.contentType("text/plain")
-        ctx.result(result.getString("text"))
+        ctx.result(text)
     } else {
         ctx.result("Not found")
         ctx.status(410)
@@ -35,18 +34,7 @@ fun viewRaw(ctx: Context) {
 
 fun Javalin.gluecan() {
     this.get("/api/pastes", { ctx ->
-        val result = connection.createStatement().executeQuery("select Pastes.date, Pastes.views, Pastes.language, Pastes.id from Pastes")
-
-        val pastes = mutableListOf<Paste>()
-        while (result.next()) {
-            val id = result.getInt("id")
-            val date = result.getString("date")
-            val views = result.getInt("views")
-            val language = result.getString("language")
-            pastes += Paste(id, views, language, null, date)
-        }
-
-        ctx.json(pastes)
+        ctx.json(getAllPastes())
     }, roles(MyRole.AUTHENTICATED))
 
     this.get("/api/pastes/:id/raw", ::viewRaw)
@@ -56,21 +44,12 @@ fun Javalin.gluecan() {
     this.get("/view/:id") { ctx ->
         val id = ctx.pathParam(":id").toInt()
 
-        val updateStatement = connection.prepareStatement("update Pastes set views = views + 1 where id = ?")
-        updateStatement.setInt(1, id)
-        updateStatement.execute()
+        updateViewCount(id)
+        val paste = getPaste(id)
 
-        val retrieveStatement = connection.prepareStatement("select PasteContent.text, Pastes.date, Pastes.views, Pastes.language from Pastes inner join PasteContent on Pastes.text_id = PasteContent.rowid where Pastes.id = ?")
-        retrieveStatement.setInt(1, id)
-        val result = retrieveStatement.executeQuery()
-
-        if (result.next()) {
-            val text = result.getString("text")
-            val date = result.getString("date")
-            val views = result.getInt("views")
-            val language = result.getString("language")
+        if (paste !== null) {
             ctx.contentType("text/html")
-            ctx.result(Templater.template(Paste(id, views, language, text, date)))
+            ctx.result(Templater.template(paste))
         } else {
             ctx.result("Not found")
             ctx.status(410)
@@ -80,14 +59,7 @@ fun Javalin.gluecan() {
     this.delete("/api/pastes/:id", { ctx ->
         val id = ctx.pathParam(":id").toInt()
 
-        val stmt = connection.prepareStatement("delete from PasteContent where rowid=(select text_id from Pastes where id=?)")
-        stmt.setInt(1, id)
-        val result = stmt.executeUpdate()
-
-        if (result > 0) {
-            val stmt2 = connection.prepareStatement("delete from Pastes where id=?")
-            stmt2.setInt(1, id)
-            stmt2.executeUpdate()
+        if (deletePaste(id)) {
             ctx.status(200)
         } else {
             ctx.status(410)
@@ -95,19 +67,9 @@ fun Javalin.gluecan() {
     }, roles(MyRole.AUTHENTICATED))
 
     this.post("/api/pastes", { ctx ->
-        val id =  uniqueId()
         val text = ctx.body()
         val language = ctx.queryParam("lang")
 
-        var stmt = connection.prepareStatement("insert into PasteContent(text) values (?)")
-        stmt.setString(1, text)
-        stmt.executeUpdate()
-
-        stmt = connection.prepareStatement("insert into Pastes(id, text_id, language) values(?, last_insert_rowid(), ?)")
-        stmt.setInt(1, id)
-        stmt.setString(2, language)
-        stmt.executeUpdate()
-
-        ctx.result(id.toString())
+        ctx.result(createPaste(text, language).toString())
     }, roles(MyRole.AUTHENTICATED))
 }
