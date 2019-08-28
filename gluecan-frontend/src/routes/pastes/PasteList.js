5a3ffe4225fbd3e65bb3d411a6db7f23327c6ccb
Nathan Kessler <nathant93@gmail.com>
2019-08-27 20:09:22 -0400

    Better paste search styling

--- gluecan-frontend/src/routes/pastes/PasteList.js
+++ gluecan-frontend/src/routes/pastes/PasteList.js
@@ -28,9 +28,13 @@ const useStyles = makeStyles(theme => ({
 
 const useSearchStyles = makeStyles(theme => ({
   root: {
-    margin: theme.spacing(2),
+    margin: theme.spacing(-1, 1, 0),
     padding: theme.spacing(1, 0, 1, 2),
     display: 'flex',
+    alignItems: 'center',
+    '*&>:last-child': {
+      marginLeft: 'auto',
+    },
   },
 }))
 
@@ -43,9 +47,14 @@ function CurrentSearch() {
     dispatch(clearSearch())
   }, [dispatch])
 
+  const results = search.ids && search.ids.length
+  const word = results === 1 ? 'paste' : 'pastes'
+
   return search.query ? (
     <Paper className={classes.root}>
-      <Typography>Showing pastes matching "{search.query}" </Typography>
+      <Typography>
+        {results} {word} matching "{search.query}"{' '}
+      </Typography>
       <IconButton onClick={handleSearchClear}>
         <CloseIcon />
       </IconButton>
